c3111bb14206230885bdf15baf89d77df89ea2a9
Mulde <Mads.villadsen@hotmail.com>
2018-05-23 13:12:59 +0200

    Add files via upload

--- sketch_apr18a.ino
+++ sketch_apr18a.ino
@@ -17,6 +17,7 @@ void setup() {
   pinMode(servo_fuck, OUTPUT);
   pinMode(servo_ring, OUTPUT);
   pinMode(servo_pinky, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
@@ -31,11 +32,42 @@ void loop() {
   val_index = analogRead(sensor_index);
   val_fuck = analogRead(sensor_fuck);
   val_ring = analogRead(sensor_ring);
-  val_pinky = analogRead(sensor_pinky);
+  val_pinky = analogRead(sensor_pinky); 
+
+  Serial.println("Thumb");
+  Serial.println(val_thumb);
+  Serial.println("index");
+  Serial.println(val_index);
+  Serial.println("fuck");
+  Serial.println(val_fuck);
+  Serial.println("ring");
+  Serial.println(val_ring);
+  Serial.println("pinky");
+  Serial.println(val_pinky);
+  delay(5000);
   
-  analogWrite(servo_thumb, val_thumb/4);
-  analogWrite(servo_index, val_index/4);
-  analogWrite(servo_fuck, val_fuck/4);
-  analogWrite(servo_ring, val_ring/4);
-  analogWrite(servo_pinky, val_pinky/4);
+
+  int thumb_map = (val_thumb, 490, 530, 255, 0 );
+  int index_map = (val_index, 480, 550, 255, 0);
+  int fuck_map = (val_fuck, 480, 550, 255, 0 );
+  int ring_map = (val_ring, 490, 560, 255, 0 );
+  int pinky_map = (val_pinky, 500, 580, 255, 0 );
+  
+  Serial.println("Thumb mapped");
+  Serial.println(thumb_map);
+  Serial.println("index mapped");
+  Serial.println(index_map);
+  Serial.println("fuck mapped");
+  Serial.println(fuck_map);
+  Serial.println("ring mapped");
+  Serial.println(ring_map);
+  Serial.println("pinky mapped");
+  Serial.println(pinky_map);
+  delay(5000);
+  
+  analogWrite(servo_thumb, val_thumb);
+  analogWrite(servo_index, val_index);
+  analogWrite(servo_fuck, val_fuck);
+  analogWrite(servo_ring, val_ring);
+  analogWrite(servo_pinky, val_pinky);
 }
